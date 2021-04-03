fdc3b3e5d8caaaadd507726e6b2b6af7fedc7c6e
vovkahimik <vovkahimik@gmail.com>
2021-04-03 15:31:15 +0300

    UseInput hook

---
+++ src/stories/hooks/UseInput.stories.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+import {useInput} from "./UseInput";
+
+export default {
+    title: 'Hooks/Input'
+}
+
+export const Input = () => {
+
+    const username = useInput('')
+    const password = useInput('')
+
+    return (
+        <div>
+            <input {...username} type={'text'} placeholder={'username'}/>
+            <input {...password} type={'text'} placeholder={'password'}/>
+            <button onClick={() => console.log(username.value, password.value)}>Click</button>
+        </div>
+    )
+}
